ee789aa41ae838021764a6b44776d8bfb39e6b9f
asela123 <31550082+asela123@users.noreply.github.com>
2017-09-01 22:11:45 +0300

    Add files via upload

---
+++ 3d.py
@@ -0,0 +1,113 @@
+#-------------------------------------------------------------------------------
+# Name:        3d
+# Purpose:
+#
+# Author:      ?????_?????
+#
+# Created:     30/08/2017
+# Copyright:   (c) ?????_????? 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+from mpl_toolkits.mplot3d import Axes3D
+import mpl_toolkits.mplot3d as a3
+import matplotlib.colors as colors
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+import matplotlib.pyplot as plt
+import random
+import pygame
+import OpenGL
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+vertices= (
+    (1, -1, -1),
+    (1, 1, -1),
+    (-1, 1, -1),
+    (-1, -1, -1),
+    (1, -1, 1),
+    (1, 1, 1),
+    (-1, -1, 1),
+    (-1, 1, 1)
+    )
+
+
+edges = (
+    (0,1),
+    (0,2),
+    (0,3),
+    (0,4),
+    (2,1),
+    (2,3),
+    (2,7),
+    (6,3),
+    (6,4),
+    (6,7),
+    (5,1),
+    (5,4),
+    (5,7)
+    )
+
+
+def Cube():
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(vertices[vertex])
+        for vertex in edge:
+            big_vertex = map(lambda x: x * 2, vertices[vertex])
+            glVertex3fv(big_vertex)
+    glEnd()
+
+
+
+def rand_pol():
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    # x = [0,1,1,0]
+    # y = [0,0,1,1]
+    # z = [0,1,0,1]
+    # verts = [zip(x, y,z)]
+    # ax.add_collection3d(Poly3DCollection(verts))
+    # plt.show()
+    for i in range(10):
+        x = rand3()
+        y = rand3()
+        z = rand3()
+        vtx = [x,y,z]
+        tri = a3.art3d.Poly3DCollection([vtx])
+        tri.set_color(colors.rgb2hex(rand3()))
+        tri.set_edgecolor('k')
+        ax.add_collection3d(tri)
+    plt.show()
+    pass
+
+
+def rand3():
+        x = random.uniform(0, 1)
+        y = random.uniform(0, 1)
+        z = random.uniform(0, 1)
+        return [x,y,z]
+
+
+
+if __name__ == '__main__':
+    # rand_pol()
+    pygame.init()
+    display = (800,600)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+    glTranslatef(0.0,0.0, -5)
+    glScalef(0.5,0.5,0.5)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        glRotatef(1, 3, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        Cube()
+        pygame.display.flip()
+        pygame.time.wait(10)
---
+++ pic_by_date1.py
@@ -0,0 +1,53 @@
+﻿#-------------------------------------------------------------------------------
+# Name:module1
+# Purpose:
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import os, os.path
+import time
+import os, winshell
+from win32com.client import Dispatch
+import win32api
+import re
+shell = Dispatch('WScript.Shell')
+lnk_re = re.compile(".*lnk$")
+
+def link_from_dir(SOME_DIR = r"C:\\temp\pics_test", LINK_TO_DIR = "", depth = 0):
+    if (LINK_TO_DIR == ""):
+        LINK_TO_DIR = SOME_DIR
+    max_depth = 10
+    print "searcging in dir "+ SOME_DIR + " Linking to dir:" + LINK_TO_DIR +" with depth  {0:}".format(depth)
+    if (depth > max_depth):
+        return(False)
+    for filename in os.listdir(SOME_DIR):
+        if (lnk_re.match(filename)):
+            print filename + " is shrtct, skipping"
+            continue
+        fullpath = os.path.join(SOME_DIR, filename)
+        if os.path.isdir(fullpath):
+            dp = depth+1
+            link_from_dir(SOME_DIR = fullpath, LINK_TO_DIR = LINK_TO_DIR,depth = dp)
+            continue
+        mtm = os.path.getmtime(fullpath)
+        # strtm = time.strftime('%m/%d/%Y', time.gmtime(mtm))
+        lnk_dir_name = time.strftime('pics_%Y_%m', time.gmtime(mtm))
+        print filename+" will be linked to  "+lnk_dir_name
+        dirpath = os.path.join(LINK_TO_DIR, lnk_dir_name)
+        if not os.path.exists(dirpath):
+            os.makedirs(dirpath)
+        filename_lnk = "{0:}{1:}".format(filename,'.lnk')
+        flinkpath = os.path.join(dirpath, filename_lnk)
+        if os.path.isfile(flinkpath):
+            print "Link Already exists, skipping"
+            next
+        # Now create the shortcut:
+        shortcut = shell.CreateShortCut(flinkpath)
+        shortcut.Targetpath = fullpath
+        shortcut.WorkingDirectory = SOME_DIR
+        # shortcut.IconLocation = icon
+        shortcut.save()
+    pass
+
+if __name__ == '__main__':
+    # link_from_dir()
+    link_from_dir(SOME_DIR = r"D:\aaa\restore_debris", LINK_TO_DIR = "D:\pic_links")
